344b3c547e376a9af982e6dd054190208841f1e2
MatthewPondDEV <matthewpondDEV@gmail.com>
2024-04-23 13:59:29 -0400

    Reverse int problem, submission beats 70% of JS users on time

---
+++ ReverseInteger.js
@@ -0,0 +1,40 @@
+/*
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+ 
+
+Constraints:
+
+-2**31 <= x <= 2**31 - 1
+*/
+
+var reverse = function (x) {
+  let str = Math.abs(x).toString();
+  let reverse = Number(Math.abs(x).toString().split("").reverse().join(""));
+
+  if (reverse >= 2 ** 31 - 1 || reverse <= (-2) ** 31) {
+    return 0;
+  }
+
+  if (x < 0) {
+    return reverse - reverse * 2;
+  } else {
+    return reverse;
+  }
+};
