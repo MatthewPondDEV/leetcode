3f484713b3e29d005349c0138e9577578b8172bc
MatthewPondDEV <matthewpondDEV@gmail.com>
2024-08-05 14:02:42 -0400

    new way to add two linked lists using dummy node

--- AddTwoNumbers.js
+++ AddTwoNumbers.js
@@ -47,3 +47,22 @@ var addTwoNumbers = function (l1, l2, carry) {
   carry = Math.floor(total / 10);
   return new ListNode(total % 10, addTwoNumbers(l1?.next, l2?.next, carry));
 };
+
+var addTwoNumbers = function (l1, l2) {
+  let total = (l1.val ? l1.val : 0) + (l2.val ? l2.val : 0);
+  let carry = Math.floor(total / 10);
+  let result = new ListNode(total % 10);
+  let temp = result;
+  l1 = l1.next;
+  l2 = l2.next;
+
+  while (l1 || l2 || carry) {
+    total = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + (carry || 0);
+    carry = Math.floor(total / 10);
+    temp.next = new ListNode(total % 10, null);
+    temp = temp.next;
+    l1 = l1 ? l1.next : null;
+    l2 = l2 ? l2.next : null;
+  }
+  return result;
+};
